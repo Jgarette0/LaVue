c36cdb27969fefd23147f9aec2c4f1ed9017f720
Jgarette0 <aajiliangarette@gmail.com>
2024-07-07 17:01:43 +0800

    new: LaVue stack :star:

---
+++ resources/js/Layout/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header>
+        <!-- Your header content here -->
+        <nav>
+            <!-- Navigation items -->
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
+</template>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "@/Layout/MainLayout.vue";
+</script>
+
+<template>
+    <MainLayout>
+        <div>
+            <!--home page content -->
+        </div>
+    </MainLayout>
+</template>
